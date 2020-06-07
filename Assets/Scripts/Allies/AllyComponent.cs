3f6da767b0f4384a69ecaac63b91d6810128f145
Vuiren <mr.vuiren@gmail.com>
2020-06-08 00:11:28 +0500

    Revert "Clearing Old Scripts"
    This reverts commit 183a3fc6881d62ac7a1209a0a3836be52054db70.

---
+++ Assets/Scripts/Allies/AllyComponent.cs
@@ -0,0 +1,7 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+public abstract class AllyComponent : WarriorComponent
+{
+
+}
---
+++ Assets/Scripts/Allies/BecomeInactive.cs
@@ -0,0 +1,22 @@
+﻿public class BecomeInactive : WarriorComponent
+{
+	protected override void SubscribeToEventsProxy()
+	{
+		eventsProxy.OnDeath += DoBecameInactive;
+	}
+
+	internal override void DisableComponent()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	internal override void EnableComponent()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void DoBecameInactive()
+	{
+		gameObject.SetActive(false);
+	}
+}
